b21fa8abbf9152fbe022785a81d9cd330ba53a6a
yesmine16 <yesmineayed@yahoo.fr>
2022-03-09 09:48:37 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.java.telnet {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.java.telnet to javafx.fxml;
+    exports com.java.telnet;
+}
